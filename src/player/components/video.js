3b8bd39191a17282093eb56c5a6e0da1f1e51f83
cascharly <cascharly@gmail.com>
2019-10-21 18:19:22 -0300

    clase 28

---
+++ src/player/components/video.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './video.css';
+
+class Video extends Component {
+    render (){
+        return (
+            <div className="Video">
+                <video
+                    autoPlay={this.props.autoplay}
+                    src={this.props.src}>
+                </video>
+            </div>
+        )
+    }
+}
+
+export default Video;
